67fa62eb7216d768415e8ae462d9546224dcb6de
jluckmann-notebook <jluckmann@udec.cl>
2025-05-05 00:42:54 -0400

    .

--- cuentasclaras/settings.py
+++ cuentasclaras/settings.py
@@ -113,6 +113,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Whitenoise
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
